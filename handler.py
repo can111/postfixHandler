33b6795c3587a3772e8132c3e40c947a04cdbc25
Umut Erten <me@uce.xyz>
2022-02-11 19:52:34 +0300

    basic def for handler part

---
+++ handler.py
@@ -0,0 +1,69 @@
+from aiohttp import web
+
+
+class QuarantineHandler(web.View):
+
+
+class NotificationHandler(web.View):
+
+
+class DatabaseHandler(web.View):
+
+
+class ConnectionHandler(web.View):
+
+
+class TransportHandler(web.View):
+
+
+class MasqHandler(web.View):
+
+
+class SmtpHandler(web.View):
+
+
+class ProxyHandler(web.View):
+
+
+class AliasHandler(web.View):
+
+
+web_api_init = {
+    'route': [{
+        'uri': '/postfix/alias',
+        'handler': AliasHandler,
+        'label': 'Alias'
+    }, {
+        'uri': '/postfix/proxy',
+        'handler': ProxyHandler,
+        'label': 'Proxy'
+    }, {
+        'uri': '/postfix/smtp',
+        'handler': SmtpHandler,
+        'label': 'Smtp'
+    }, {
+        'uri': '/postfix/masq',
+        'handler': MasqHandler,
+        'label': 'masq'
+    }, {
+        'uri': '/postfix/transport',
+        'handler': TransportHandler,
+        'label': 'Transport'
+    }, {
+        'uri': '/postfix/connect',
+        'handler': ConnectionHandler,
+        'label': 'Connect'
+    }, {
+        'uri': '/postfix/database',
+        'handler': DatabaseHandler,
+        'label': 'Database'
+    }, {
+        'uri': '/postfix/notification',
+        'handler': NotificationHandler,
+        'label': 'Notification'
+    }, {
+        'uri': '/postfix/quarantine',
+        'handler': QuarantineHandler,
+        'label': 'Quarantine'
+    }]
+}
