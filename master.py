000621036b92ad8709d2abeb58b0bb825c6e2ffe
Umut Erten <me@uce.xyz>
2022-02-11 19:36:08 +0300

    method naming for postconf

--- master.py
+++ master.py
@@ -6,3 +6,16 @@ import asyncio.subprocess
 class Master:
     def __init__(self):
         self.postconf = '/usr/sbin/postconf'
+
+    async def basic_process(self, cmd, params: list) -> (bytes, bytes, int):
+        pass
+
+    async def get_postconf(self, info: dict):
+        pass
+
+    async def del_postconf(self, info: dict):
+        pass
+
+    async def put_postconf(self, info: dict):
+        pass
+
